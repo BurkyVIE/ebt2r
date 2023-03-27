353429e81e6c6751e5f21f2a88f635cd68501a15
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2023-03-27 15:25:00 +0200

    Update rate_serial.r

--- rate_serial.r
+++ rate_serial.r
@@ -4,27 +4,34 @@ rate_serial <- function(serial = "XY0123456789") {
   D1_raw = abs(diff(DigitVector))
   D1_raw[D1_raw > 5] = 10 - D1_raw[D1_raw > 5]
   D1_raw = as.integer(D1_raw)
+  Solid = as.integer(max(0, nchar(str_extract_all(serial, "(\\d)\\1+")[[1]]), na.rm = TRUE))
+  Ladder = max(table(cumsum(D1_raw != 1)))
+  Unique = length(unique(DigitVector))
+  Radar = identical(DigitVector[1:5], rev(DigitVector)[1:5])
+  Binary = Unique == 2
   D1 = 1 - sum(D1_raw) / 5 / (Length - 1)
   D2_raw = as.integer(abs(diff(D1_raw)))
   D2 = 1 - sum(D2_raw) / 5 / (Length - 2)
-  Unique = length(unique(DigitVector))
+  SF = sqrt(Solid / (Length - 1))
   UF = c(2, 1, .5, .25, .125, 0, 0, 0, 0, .5)[Unique]
-  Radar = identical(DigitVector[1:5], rev(DigitVector)[1:5])
-  Binary = Unique == 2
   
   return(
     tibble(Serial = serial,
-           DigitVector = list(DigitVector),
+           # DigitVector = list(DigitVector),
            Length = Length,
-           D1_raw = list(D1_raw),
+           # D1_raw = list(D1_raw),
            D1 = D1,
-           D2_raw = list(D2_raw),
+           # D2_raw = list(D2_raw),
            D2 = D2,
-           Unique = Unique,
+           SF = SF,
            UF = UF,
+           Solid = Solid,
+           Ladder = Ladder,
+           Unique = Unique,
            Radar = Radar,
            Binary = Binary,
-           Rating2 = (2 * D1 + D2 + UF) * (1 + .5 * (Radar | Binary)) / 4
+           Rating = Solid/10 + Ladder/10 + (1-Unique/10) + .3 * Radar + .5 * Binary,
+           Rating2 = (2 * D1 + D2 + SF + UF) * (1 + .5 * (Radar | Binary)) / 5
     )
   )
 }
